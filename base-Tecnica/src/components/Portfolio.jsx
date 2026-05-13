ab19e5a7a35c194eb651be7bebef59455f128192
Guilherme cruz alves <nestunit980@gmail.com>
2026-05-13 01:11:25 -0300

    adicionando uma area de eventos já feitos

---
+++ base-Tecnica/src/components/Portfolio.jsx
@@ -0,0 +1,29 @@
+import siteImag from '../assets/projeto1.jpg'
+import sistemaImag from '../assets/projeto2.jpg'
+import portifolioImag from '../assets/projeto3.jpg'
+
+function portfolio(){
+    return(
+        <section className="py-16 bg-black px-6">
+            <div className="max-w-6x1 mx-auto">
+                {/* TITULO */}
+            <div className="text-center mb-14">
+                <h2 className="text-4xl font-bold text-white">Um pouco do que podemos fazer</h2>
+            </div>
+            
+            <p className="text-gray-300 m-4">
+                Aqui estão alguns dos nossos projetos mais recentes. Cada um deles representa um compromisso com a excelência e a inovação.
+            </p>
+
+            </div>
+               {/* GRID */}
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+
+                {/* CARD 1 */}
+            <div className="bg-zinc-900 "
+
+            </div>
+
+        </section>
+    )
+}
